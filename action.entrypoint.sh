e08f6411b1bfc9fa6de220e5c649ab3c8b473c35
Nicolaj Figaw <figaw@hotmail.com>
2020-06-23 17:57:03 +0200

    ADD sanity check to action

--- action.entrypoint.sh
+++ action.entrypoint.sh
@@ -9,6 +9,7 @@ LOG="${LOG//$'\r'/'%0D'}"
 echo "::set-output name=log::$LOG"
 
 echo "::debug::$LOG"
+echo "::debug::THIS IS A SANITY CHECK.."
 
 echo $LOG | grep "^.*A detailed description of the rules.*" > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
