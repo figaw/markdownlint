3049c8af233d1df64b8306d0d5f98baa5028e908
Nicolaj Figaw <figaw@hotmail.com>
2020-06-23 18:10:52 +0200

    TRY using default image in action

--- action.entrypoint.sh
+++ action.entrypoint.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 
-LOG=$(mdl $INPUT_PARAMETERS)
-
-LOG="${LOG//'%'/'%25'}"
-LOG="${LOG//$'\n'/'%0A'}"
-LOG="${LOG//$'\r'/'%0D'}"
-
-echo "::set-output name=log::$LOG"
-
-echo "::debug::$LOG"
-echo "::debug::THIS IS A SANITY CHECK.."
-
-echo $LOG | grep "^.*A detailed description of the rules.*" > /dev/null 2>&1
-if [ "$?" -eq "0" ]; then
-    exit 1
-fi
-
-
-exit 0
+mdl "$@"
